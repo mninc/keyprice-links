c56e6990d93984b8238b9a2f546f06107a9cec37
mninc <manicminer106@gmail.com>
2019-08-13 17:31:59 +0100

    made a kind of hacky way to put the time back into the url

--- keyprice-links.user.js
+++ keyprice-links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         marketplace.tf keyprice links
 // @namespace    http://manic.tf
-// @version      2.0
+// @version      3.0
 // @description  add http://manic.tf/keyprice links to marketplace.tf detailed stats popups
 // @author       manic
 // @grant        none
@@ -18,40 +18,51 @@
 (function() {
     'use strict';
 
-    function addLink() {
-        let tables = document.getElementsByTagName("table");
-        for (let i = 0; i < tables.length; i++) {
-            let table = tables[i];
-            if (table.id.startsWith("salesTable")) {
-                let body = table.getElementsByTagName("tbody")[0];
-                let total = 0;
-                for (let i = 0; i < body.children.length; i++) {
-                    total += parseInt(body.children[i].children[1].innerText.replace(",", ""));
-                }
-                let median = Math.floor(total / 2);
-                let index = 0;
-                let price;
-                for (let i = 0; i < body.children.length; i++) {
-                    index += parseInt(body.children[i].children[1].innerText.replace(",", ""));
-                    if (index > median) {
-                        price = body.children[i].children[0].innerText.substr(1);
-                        break;
-                    }
-                    if (i === body.children.length - 1) {
-                        price = body.children[i].children[0].innerText.substr(1);
-                    }
-                }
+    function run() {
+        let elements = document.getElementsByClassName('panel-body');
+        let panel = elements[1];
+        let script = panel.getElementsByTagName("script")[0];
+        let canvas = panel.getElementsByTagName('canvas')[0];
+        let canvasID = canvas.id;
+        let canvasSku = canvas.dataset.sku;
+        let iframe = panel.getElementsByTagName("iframe")[0];
 
-                open = true;
+        let scriptText = script.innerText;
+        scriptText = scriptText.substring(0, scriptText.indexOf('});'));
+        scriptText += `
+            let time, price;
+    
+            myLineChart.options.tooltips.custom = function(tooltip){
+                if (!tooltip.title) return;
+                let date = tooltip.title[0];
+    
+                time = new Date(date);
+    
+                let year = time.getFullYear().toString();
+                let month = time.getMonth() + 1;
+                month = (month>9 ? '' : '0') + month;
+                month = month.toString();
+                let day = time.getDate();
+                day = (day>9 ? '' : '0') + day;
+                day = day.toString();
+    
+                time = year + "-" + month + "-" + day;
+                price = tooltip.body[0].lines[0].substring(14);
+            };
+    
+            let old_function = myLineChart.options.onClick;
+            myLineChart.options.onClick = function(data, elements) {
+                old_function(data, elements);
+    
                 if (document.getElementById("keyprice-link")) {
-                    document.getElementById("keyprice-link").href = "http://manic.tf/keyprice?price=" + price;
+                    document.getElementById("keyprice-link").href = "http://manic.tf/keyprice?at=" + time + "&price=" + price;
                 } else {
                     let a = document.createElement("a");
-                    a.href = "http://manic.tf/keyprice?price=" + price;
+                    a.href = "http://manic.tf/keyprice?at=" + time + "&price=" + price;
                     a.innerText = "Convert to keys";
                     a.id = "keyprice-link";
                     a.target = "_blank";
-
+    
                     let titles = document.getElementsByClassName("modal-title");
                     for (let i = 0; i < titles.length; i++) {
                         let title = titles[i];
@@ -61,33 +72,23 @@
                         }
                     }
                 }
-            }
-        }
+            };
+            myLineChart.update();`;
+        scriptText += '});';
+
+        let newCanvas = document.createElement('canvas');
+        newCanvas.id = canvasID;
+        newCanvas.dataset.sku = canvasSku;
+        newCanvas.style.width = "100%";
+        newCanvas.style.height = "305";
+        let newScript = document.createElement("script");
+        newScript.innerHTML = scriptText;
+        canvas.remove();
+        iframe.remove();
+        script.remove();
+        panel.appendChild(newCanvas);
+        panel.appendChild(newScript);
 
     }
-
-    let modal;
-    function getModal() {
-        let elements = document.querySelectorAll('*[id]');
-        for (let i = 0; i < elements.length; i++) {
-            let elem = elements[i];
-            if (elem.id.startsWith("salesTableModal")) {
-                modal = elem;
-                clearInterval(interval);
-                setInterval(check, 200);
-                break;
-            }
-        }
-    }
-    let interval = setInterval(getModal, 1000);
-    let open = false;
-
-    function check() {
-        if (!open && modal.style.display === "block") {
-            addLink();
-        } else if (modal.style.display !== "block") {
-            open = false;
-        }
-    }
-
+    window.onload = run;
 })();
