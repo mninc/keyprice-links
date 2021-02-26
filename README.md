dce2ec6fba7ea2af8deec7e7832732859c30002c
mninc <manicminer106@gmail.com>
2021-02-26 16:16:27 +0000

    4.0: Support mannco.store item pages

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # keyprice links
-Add https://manic.tf/keyprice links to marketplace.tf detailed stats popups
+Add https://manic.tf/keyprice links to marketplace.tf/mannco.store item pages
 
 ## Installation
 1. Install [Greasemonkey (Firefox)](http://www.greasespot.net/) or [Tampermonkey (Chrome)](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
 2. Open the [script in raw view](https://github.com/mninc/keyprice-links/raw/master/keyprice-links.user.js).
 3. Install.
 
+## Usage
+
+### marketplace.tf
+Click on any point on the graph, then click "Convert to keys".
+![Convert to keys](https://cdn.discordapp.com/attachments/337943916613599234/814893173586395226/unknown.png)
+
+### mannco.store
+Click on any point on the graph.
+
 ## Verify Accuracy and Authenticity
-Want to verify the accuracy and authenticity of this script and https://manic.tf/keyprice? Go to the [marketplace.tf key page](https://marketplace.tf/items/tf2/5021;6) and click on any point in the graph. When you click "Convert to keys" the displayed value should always be 1.00 keys.
+Want to verify the accuracy and authenticity of this script and https://manic.tf/keyprice? 
 
-## In use
-![Example](https://cdn.discordapp.com/attachments/445162468541464576/610949876502757376/unknown.png)
+### marketplace.tf
+Go to the [marketplace.tf key page](https://marketplace.tf/items/tf2/5021;6) and click on any point in the graph. When you click "Convert to keys" the keyprice for that day should be the same as what was on the graph.
 
-![Example 2](https://cdn.discordapp.com/attachments/445162468541464576/610950045969416203/unknown.png)
+### mannco.store
+Go to the [mannco.store key page](https://mannco.store/item/1) and click on any point in the graph. The keyprice for that day should be the same as what was on the graph.
--- keyprice-links.user.js
+++ keyprice-links.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         marketplace.tf keyprice links
+// @name         keyprice links
 // @namespace    http://manic.tf
-// @version      3.3
-// @description  add http://manic.tf/keyprice links to marketplace.tf detailed stats popups
+// @version      4.0
+// @description  add http://manic.tf/keyprice links to marketplace.tf/mannco.store item pages
 // @author       manic
 // @grant        none
 // @license      MIT
@@ -13,90 +13,105 @@
 
 // @run-at       document-end
 // @match        https://marketplace.tf/items/*
+// @match        https://mannco.store/item/*
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    let elements = document.getElementsByClassName('panel-body');
-    let panel;
-    for (let i = 0; i < elements.length; i++) {
-        let elem = elements[i];
-        let scriptFound = elem.getElementsByTagName("script").length;
-        let canvasFound = elem.getElementsByTagName("canvas").length;
-        if (canvasFound && scriptFound) {
-            panel = elem;
-            break;
+    if (window.location.hostname.includes("marketplace.tf")) {
+        let elements = document.getElementsByClassName('panel-body');
+        let panel;
+        for (let i = 0; i < elements.length; i++) {
+            let elem = elements[i];
+            let scriptFound = elem.getElementsByTagName("script").length;
+            let canvasFound = elem.getElementsByTagName("canvas").length;
+            if (canvasFound && scriptFound) {
+                panel = elem;
+                break;
+            }
         }
-    }
-    if (!panel) return;
-    let script = panel.getElementsByTagName("script")[0];
-    let canvas = panel.getElementsByTagName('canvas')[0];
-    let canvasID = canvas.id;
-    let newCanvasID = canvasID + "-manic";
-    let canvasSku = canvas.dataset.sku;
-    let iframe = panel.getElementsByTagName("iframe")[0];
+        if (!panel) return;
+        let script = panel.getElementsByTagName("script")[0];
+        let canvas = panel.getElementsByTagName('canvas')[0];
+        let canvasID = canvas.id;
+        let newCanvasID = canvasID + "-manic";
+        let canvasSku = canvas.dataset.sku;
+        let iframe = panel.getElementsByTagName("iframe")[0];
 
-    let scriptText = script.innerText;
-    scriptText = scriptText.substring(0, scriptText.indexOf('});'));
-    scriptText = scriptText.replace(new RegExp(canvasID, "g"), newCanvasID);
-    scriptText += `
-        let time, price;
-
-        myLineChart.options.tooltips.custom = function(tooltip){
-            if (!tooltip.title) return;
-            let date = tooltip.title[0];
-
-            time = new Date(date);
-
-            let year = time.getFullYear().toString();
-            let month = time.getMonth() + 1;
-            month = (month>9 ? '' : '0') + month;
-            month = month.toString();
-            let day = time.getDate();
-            day = (day>9 ? '' : '0') + day;
-            day = day.toString();
-
-            time = year + "-" + month + "-" + day;
-            price = tooltip.body[0].lines[0].substring(14);
-        };
-
-        let old_function = myLineChart.options.onClick;
-        myLineChart.options.onClick = function(data, elements) {
-            old_function(data, elements);
-
-            if (document.getElementById("keyprice-link")) {
-                document.getElementById("keyprice-link").href = "http://manic.tf/keyprice?at=" + time + "&price=" + price;
-            } else {
-                let a = document.createElement("a");
-                a.href = "http://manic.tf/keyprice?at=" + time + "&price=" + price;
-                a.innerText = "Convert to keys";
-                a.id = "keyprice-link";
-                a.target = "_blank";
-
-                let titles = document.getElementsByClassName("modal-title");
-                for (let i = 0; i < titles.length; i++) {
-                    let title = titles[i];
-                    if (title.innerText === "Detailed Stats") {
-                        title.innerText += "  -  ";
-                        title.appendChild(a);
+        let scriptText = script.innerText;
+        scriptText = scriptText.substring(0, scriptText.indexOf('});'));
+        scriptText = scriptText.replace(new RegExp(canvasID, "g"), newCanvasID);
+        scriptText += `
+            let time, price;
+    
+            myLineChart.options.tooltips.custom = function(tooltip){
+                if (!tooltip.title) return;
+                let date = tooltip.title[0];
+    
+                time = new Date(date);
+    
+                let year = time.getFullYear().toString();
+                let month = time.getMonth() + 1;
+                month = (month>9 ? '' : '0') + month;
+                month = month.toString();
+                let day = time.getDate();
+                day = (day>9 ? '' : '0') + day;
+                day = day.toString();
+    
+                time = year + "-" + month + "-" + day;
+                price = tooltip.body[0].lines[0].substring(14);
+            };
+    
+            let old_function = myLineChart.options.onClick;
+            myLineChart.options.onClick = function(data, elements) {
+                old_function(data, elements);
+    
+                if (document.getElementById("keyprice-link")) {
+                    document.getElementById("keyprice-link").href = "http://manic.tf/keyprice?site=marketplace.tf&at=" + time + "&price=" + price;
+                } else {
+                    let a = document.createElement("a");
+                    a.href = "http://manic.tf/keyprice?at=" + time + "&price=" + price;
+                    a.innerText = "Convert to keys";
+                    a.id = "keyprice-link";
+                    a.target = "_blank";
+    
+                    let titles = document.getElementsByClassName("modal-title");
+                    for (let i = 0; i < titles.length; i++) {
+                        let title = titles[i];
+                        if (title.innerText === "Detailed Stats") {
+                            title.innerText += "  -  ";
+                            title.appendChild(a);
+                        }
                     }
                 }
-            }
-        };
-        myLineChart.update();`;
-    scriptText += '});';
+            };
+            myLineChart.update();`;
+        scriptText += '});';
 
-    let newCanvas = document.createElement('canvas');
-    newCanvas.id = newCanvasID;
-    newCanvas.dataset.sku = canvasSku;
-    newCanvas.width = canvas.width;
-    newCanvas.height = canvas.height;
-    let newScript = document.createElement("script");
-    newScript.innerHTML = scriptText;
-    canvas.remove();
-    iframe.remove();
-    script.remove();
-    panel.appendChild(newCanvas);
-    panel.appendChild(newScript);
+        let newCanvas = document.createElement('canvas');
+        newCanvas.id = newCanvasID;
+        newCanvas.dataset.sku = canvasSku;
+        newCanvas.width = canvas.width;
+        newCanvas.height = canvas.height;
+        let newScript = document.createElement("script");
+        newScript.innerHTML = scriptText;
+        canvas.remove();
+        iframe.remove();
+        script.remove();
+        panel.appendChild(newCanvas);
+        panel.appendChild(newScript);
+    } else {
+        let time, price;
+        window.myLine.options.tooltips.custom = function(tooltip) {
+            if (!tooltip.title) return;
+
+            time = tooltip.title[0];
+            price = tooltip.body[0].lines[0].substring(14);
+        }
+        window.myLine.options.onClick = function() {
+            console.log(time, price);
+            window.open(`https://manic.tf/keyprice?site=mannco.store&at=${time}&price=${price}`)
+        }
+    }
 })();
